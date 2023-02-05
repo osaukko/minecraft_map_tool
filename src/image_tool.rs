0a5c5358a066a94182ff093301ab68583fc8ceb5
Ossi Saukko <osaukko@gmail.com>
2023-02-05 12:55:08 +0200

    Show Maps In Terminal
    This commit adds the viuer crate to display maps in a terminal when
    iTerm, Kitty, or Sixel graphics protocol is available.

--- src/image_tool.rs
+++ src/image_tool.rs
@@ -1,4 +1,5 @@
 use clap::Args;
+use image::DynamicImage;
 use minecraft_map_tool::palette::{generate_palette, BASE_COLORS_2699};
 use minecraft_map_tool::MapItem;
 use std::path::PathBuf;
@@ -12,6 +13,10 @@ pub struct ImageArgs {
     /// Write the map image to the file. Standard file formats are supported.
     #[arg(short, long)]
     output_file: Option<PathBuf>,
+
+    /// Show map in terminal using iTerm, Kitty, or Sixel graphics protocol
+    #[arg(short, long, group = "term")]
+    show_in_terminal: bool,
 }
 
 pub fn create_image(args: &ImageArgs) -> ExitCode {
@@ -30,6 +35,20 @@ pub fn create_image(args: &ImageArgs) -> ExitCode {
             return ExitCode::FAILURE;
         }
     };
+
+    if args.show_in_terminal {
+        let config = viuer::Config {
+            absolute_offset: false,
+            transparent: true,
+            truecolor: true,
+            ..Default::default()
+        };
+        let dynamic_image = DynamicImage::from(image.clone());
+        if let Err(err) = viuer::print(&dynamic_image, &config) {
+            eprintln!("Could not show image: {err}");
+            return ExitCode::FAILURE;
+        }
+    }
 
     if let Some(output_file) = &args.output_file {
         match image.save(output_file) {
